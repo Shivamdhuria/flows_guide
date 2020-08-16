74c0dcc93135eef62d990f579a9137b9a5d16042
Shivam Dhuria <sdhuria@ask-fast.com>
2020-08-17 00:31:22 +0530

    clean up

--- app/src/main/java/com/example/flows/dogDetail/DogDetailFragment.kt
+++ app/src/main/java/com/example/flows/dogDetail/DogDetailFragment.kt
@@ -1,15 +1,9 @@
 package com.example.flows.dogDetail
 
-import android.graphics.drawable.Drawable
 import android.os.Bundle
 import android.view.View
-import android.widget.ImageView
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.navArgs
-import com.bumptech.glide.Glide
-import com.bumptech.glide.load.DataSource
-import com.bumptech.glide.load.engine.GlideException
-import com.bumptech.glide.request.RequestListener
 import com.example.flows.R
 import com.example.flows.extensions.themeColor
 import com.example.flows.util.ImageLoader
@@ -25,13 +19,8 @@ class DogDetailFragment : Fragment(R.layout.dog_detail_fragment) {
     override fun onCreate(savedInstanceState: Bundle?) {
 
         sharedElementEnterTransition = MaterialContainerTransform().apply {
-            // Scope the transition to a view in the hierarchy so we know it will be added under
-            // the bottom app bar but over the elevation scale of the exiting HomeFragment.
             drawingViewId = R.id.nav_host_fragment
             duration = resources.getInteger(R.integer.motion_duration_large).toLong()
-
-//            scrimColor = Color.TRANSPARENT
-//            this.pathMotion = com.google.android.material.transition.MaterialArcMotion()
             isElevationShadowEnabled = true
             setAllContainerColors(requireContext().themeColor(R.attr.colorSurface))
         }
@@ -45,60 +34,5 @@ class DogDetailFragment : Fragment(R.layout.dog_detail_fragment) {
         detail_container.transitionName = imageUrl
         ImageLoader.loadImage(requireContext(), imageUrl, image_dog_detail)
         textview_dog_breed.text = breed
-
-//        setSharedElementTransitionOnEnter()
-//        postponeEnterTransition()
-
-//        image_view_full_screen_doggo.apply {
-//            transitionName = imageUrl
-//            startEnterTransitionAfterLoadingImage(imageUrl, this)
-//        }
-    }
-
-//    private fun loadLayout() {
-//        val (imageUrl, breed) = args
-//        setSharedElementTransitionOnEnter()
-//        postponeEnterTransition()
-// //        postponeEnterTransition()
-// //        ImageLoader.loadImage(requireContext(), imageUrl, image)
-//        Log.e("FRA", id.toString())
-//        image.apply {
-//            transitionName = imageUrl
-//            startEnterTransitionAfterLoadingImage(imageUrl, this)
-//        }
-//        name.text = breed
-//    }
-
-//    private fun setSharedElementTransitionOnEnter() {
-//        sharedElementEnterTransition = TransitionInflater.from(context).inflateTransition(R.transition.shared_element_transition)
-//    }
-
-    private fun startEnterTransitionAfterLoadingImage(imageAddress: String, imageView: ImageView) {
-        Glide.with(this)
-                .load(imageAddress)
-                .dontAnimate() // 1
-                .listener(object : RequestListener<Drawable> { // 2
-                    override fun onLoadFailed(
-                        e: GlideException?,
-                        model: Any?,
-                        target: com.bumptech.glide.request.target.Target<Drawable>?,
-                        isFirstResource: Boolean
-                    ): Boolean {
-                        startPostponedEnterTransition()
-                        return false
-                    }
-
-                    override fun onResourceReady(
-                        resource: Drawable,
-                        model: Any,
-                        target: com.bumptech.glide.request.target.Target<Drawable>,
-                        dataSource: DataSource,
-                        isFirstResource: Boolean
-                    ): Boolean {
-                        startPostponedEnterTransition()
-                        return false
-                    }
-                })
-                .into(imageView)
     }
 }
